a9ba3f7283665c28d7d9f82f92233ae78b51ce41
Carsten Gräf <evandor@gmail.com>
2018-04-05 19:08:08 +0200

    wip

---
+++ skysail.server.app.docker/src/io/skysail/server/app/docker/Activator.scala
@@ -0,0 +1,42 @@
+package io.skysail.server.app.docker
+
+import akka.actor.ActorSystem
+import domino.DominoActivator
+import io.skysail.server.RoutesCreatorTrait
+import io.skysail.server.app.ApplicationProvider
+import org.slf4j.LoggerFactory
+
+class Activator extends DominoActivator {
+
+  private var log = LoggerFactory.getLogger(this.getClass)
+
+ // var app: OsgiApplication = _
+  //var logServiceTracker: ServiceTracker[LogService,LogService] = _
+
+  whenBundleActive {
+
+    whenServicesPresent[RoutesCreatorTrait, ActorSystem] { (routesCreator, actorSystem) =>
+      log info s"dbService available in ${this.getClass.getName}"
+      //app = new OsgiApplication(bundleContext, routesCreator, actorSystem)
+      //app.providesService[ApplicationProvider]
+    }
+
+    //whenServicePresent[RepositoryAdmin] { repoAdmin =>
+    //whenServicesPresent[RepositoryService, RoutesCreatorTrait, ActorSystem] { (repoService, routesCreator, actorSystem) =>
+      //      log info s"RepositoryAdmin available in ${this.getClass.getName}, creating commands and SkysailObrApplication"
+      //      val cmd = new SkysailObrCommands(repoAdmin)
+      //
+      //      cmd.providesService[Object](
+      //        "osgi.command.scope" -> Constants.SKYSAIL_COMMAND_SCOPE,
+      //        "osgi.command.function" -> "listRepos")
+      //
+      //      cmd.providesService[Object](
+      //        "osgi.command.scope" -> Constants.SKYSAIL_COMMAND_SCOPE,
+      //        "osgi.command.function" -> "search")
+
+      //app = new ObrApplication(bundleContext, routesCreator, actorSystem, repoService)
+      //app.providesService[ApplicationProvider]
+    //}
+
+  }
+}
